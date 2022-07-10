500bbb35233d4802ae0801ca422ddb9a1eeec139
salvatore <salvatore.didio@gmail.com>
2022-07-10 23:26:45 +0200

    Update DummyProducts

---
+++ src/utils/ready.ts
@@ -0,0 +1,16 @@
+const ready = function (fn: any) {
+    const completed = () => {
+      document.removeEventListener("DOMContentLoaded", completed);
+      window.removeEventListener("load", completed);
+      fn();
+    };
+    if (document.readyState !== "loading") {
+      setTimeout(fn);
+    } else {
+      document.addEventListener("DOMContentLoaded", completed);
+      window.addEventListener("load", completed);
+    }
+  };
+  
+
+  export default ready;
